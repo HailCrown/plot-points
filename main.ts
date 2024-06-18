a8daad09e02a11ae7e908fcda8cb122f4d1755d4
HailCrown <ethan.nguyen007@sydstu.catholic.edu.au>
2024-06-19 09:22:34 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+led.plot(1, 0)
+led.plot(3, 0)
+led.plot(2, 1)
+led.plot(1, 3)
+led.plot(2, 3)
+led.plot(3, 3)
+basic.forever(function () {
+    led.toggle(0, 2)
+    led.toggle(4, 2)
+    basic.pause(100)
+})
